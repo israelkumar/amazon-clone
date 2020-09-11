2403840e1983c2e993e931698fcdc4b2eecd33c3
israelkumar <israelkumar@gmail.com>
2020-09-11 12:44:18 -0700

    Add files via upload

---
+++ build/precache-manifest.6bf58356bf966926cf217f82fdd731ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec4740229fe811fcfcd65ada54d36d7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e349c773c72d9aefca7d",
+    "url": "/static/css/main.16ba84a8.chunk.css"
+  },
+  {
+    "revision": "941703efc091734158a3",
+    "url": "/static/js/2.4e870c0b.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/static/js/2.4e870c0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e349c773c72d9aefca7d",
+    "url": "/static/js/main.7e4882e2.chunk.js"
+  },
+  {
+    "revision": "92a146e5ab9e0f9e785b",
+    "url": "/static/js/runtime-main.90140090.js"
+  }
+]);
